8959f561f95d86bbe5b18e4fb06558cf4cfb8554
ivanpinza <ivandajopinza@gmail.com>
2025-01-14 17:04:51 -0500

    fixed prettier

--- src/components/Home/Backend.jsx
+++ src/components/Home/Backend.jsx
@@ -10,11 +10,7 @@ export const Backend = () => {
         <div className="h-[250px] w-[300px] border-t border-b border-dotted border-white p-3">
           <h2 className="pb-2 text-xl text-center">BANK</h2>
           <p className="pt-3 font-light">
-            Un e-commerce robusto con funcionalidades CRUD para usuarios
-            verificados. Desde explorar y modificar productos hasta finalizar
-            compras, la plataforma asegura una experiencia de compra
-            personalizada y segura, adaptada para satisfacer las demandas de los
-            usuarios.
+          Es una aplicación de backend desarrollada en Node.js con SQL, que implementa operaciones CRUD utilizando peticiones GET, POST, PUT y DELETE. Permite la creación de usuarios con sus respectivas claves y la realización de transferencias de dinero entre cuenta.
           </p>
           <br></br>
           <ul className="flex gap-10 justify-center">
@@ -38,11 +34,29 @@ export const Backend = () => {
           <ul className="flex gap-10 justify-center">
             <li className="hover:text-yellow-500">
               <a href="https://github.com/pinzaivan/entregable6">
-                BANK - Git-Hub
+                REPAIRS - Git-Hub
               </a>
             </li>
           </ul>
         </div>
+        <div className="h-[250px] w-[300px] border-t border-b border-dotted border-white p-3">
+          <h2 className="pb-2 text-xl text-center">BANK</h2>
+          <p className="pt-3 font-light">
+            Un e-commerce robusto con funcionalidades CRUD para usuarios
+            verificados. Desde explorar y modificar productos hasta finalizar
+            compras, la plataforma asegura una experiencia de compra
+            personalizada y segura, adaptada para satisfacer las demandas de los
+            usuarios.
+          </p>
+          <br></br>
+          <ul className="flex gap-10 justify-center">
+            <li className="hover:text-yellow-500">
+              <a href="https://github.com/pinzaivan/entregable6">
+                MEALS - Git-Hub
+              </a>
+            </li>
+          </ul>
+        </div>    
       </section>
     </main>
   );
