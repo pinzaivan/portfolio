17b21aff73cee610bf40689fb4fb974857f8634a
ivanpinza <ivandajopinza@gmail.com>
2025-01-15 11:59:31 -0500

    add backend

--- src/components/Home/Backend.jsx
+++ src/components/Home/Backend.jsx
@@ -6,8 +6,8 @@ export const Backend = () => {
       <h1 className="className= text-yellow-500 font-semibold text-2xl mb-8">
         Proyectos Back end
       </h1>
-      <section className="grid grid-cols-3">
-        <div className="h-[300px] w-[300px] border-t border-b border-dotted border-white p-3">
+      <section className="grid grid-cols-3 gap-20">
+        <div className="h-[450px] w-[300px] border-t border-b border-dotted border-white p-3 flex flex-col items-center gap-12">
           <h2 className="pb-2 text-xl text-center">BANK</h2>
           <p className="pt-3 font-light">
             Es una aplicación de backend desarrollada en Node.js con SQL, que
@@ -16,7 +16,7 @@ export const Backend = () => {
             la realización de transferencias de dinero entre cuenta
           </p>
           <br></br>
-          <ul className="flex gap-10 justify-center">
+          <ul className="flex gap-10">
             <li className="hover:text-yellow-500">
               <a href="https://github.com/pinzaivan/Back_bank.git">
                 BANK - Git-Hub
@@ -24,38 +24,68 @@ export const Backend = () => {
             </li>
           </ul>
         </div>
-        <div className="h-[300px] w-[300px] border-t border-b border-dotted border-white p-3">
-          <h2 className="pb-2 text-xl text-center">BANK</h2>
+        <div className="h-[450px] w-[300px] border-t border-b border-dotted border-white p-3">
+          <h2 className="pb-2 text-xl text-center">MEALS</h2>
           <p className="pt-3 font-light">
-            Un e-commerce robusto con funcionalidades CRUD para usuarios
-            verificados. Desde explorar y modificar productos hasta finalizar
-            compras, la plataforma asegura una experiencia de compra
-            personalizada y segura, adaptada para satisfacer las demandas de los
-            usuarios.
+            Una aplicación de backend diseñada para gestionar restaurantes
+            mediante operaciones CRUD completas (GET, POST, PUT y DELETE). Los
+            usuarios pueden registrarse e iniciar sesión, y una vez
+            autenticados, crear, actualizar y eliminar registros de
+            restaurantes. La aplicación implementa un manejo robusto de errores
+            para garantizar la estabilidad y la seguridad del sistema, además de
+            utilizar middlewares para la validación de datos y protección de
+            rutas. Las rutas están organizadas para facilitar la escalabilidad y
+            el mantenimiento del proyecto
           </p>
           <br></br>
           <ul className="flex gap-10 justify-center">
             <li className="hover:text-yellow-500">
-              <a href="https://github.com/pinzaivan/entregable6">
+              <a href="https://github.com/pinzaivan/Back_meals.git">
                 REPAIRS - Git-Hub
               </a>
             </li>
           </ul>
         </div>
-        <div className="h-[300px] w-[300px] border-t border-b border-dotted border-white p-3">
-          <h2 className="pb-2 text-xl text-center">BANK</h2>
+        <div className="h-[450px] w-[300px] border-t border-b border-dotted border-white p-3">
+          <h2 className="pb-2 text-xl text-center">REPAIRS</h2>
           <p className="pt-3 font-light">
-            Un e-commerce robusto con funcionalidades CRUD para usuarios
-            verificados. Desde explorar y modificar productos hasta finalizar
-            compras, la plataforma asegura una experiencia de compra
-            personalizada y segura, adaptada para satisfacer las demandas de los
-            usuarios.
+            Una aplicación de backend diseñada para gestionar un taller mediante
+            operaciones CRUD completas (GET, POST, PUT y DELETE). Al crear un
+            usuario, se asigna un rol de administrador o trabajador según las
+            necesidades. Los administradores tienen la capacidad de cambiar el
+            estado de las reparaciones (lista o pendiente). Permite actualizar
+            datos de los usuarios, como el correo electrónico, con validaciones
+            y manejo de errores robusto. La arquitectura del proyecto incorpora
+            middlewares para la autenticación, autorización y validación de
+            datos y con rutas organizadas.
           </p>
           <br></br>
           <ul className="flex gap-10 justify-center">
             <li className="hover:text-yellow-500">
-              <a href="https://github.com/pinzaivan/entregable6">
-                MEALS - Git-Hub
+              <a href="https://github.com/pinzaivan/Back_repairs.git">
+                REPAIRS - Git-Hub
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="h-[450px] w-[300px] border-t border-b border-dotted border-white p-3">
+          <h2 className="pb-2 text-xl text-center">VALIDADOR</h2>
+          <p className="pt-3 font-light">
+            Una herramienta desarrollada en Python 2.7 para realizar
+            validaciones alfanuméricas en los datos almacenados dentro de una
+            Geodatabase (GDB) en ArcGIS 10.8. Este validador examina cada
+            registro y genera un archivo CSV como resultado, detallando los
+            códigos prediales, la capa donde se realizó la verificación, el
+            estado del registro (válido o inválido) y la razón específica en
+            caso de ser inválido. La herramienta está diseñada para automatizar
+            la revisión de datos, garantizando la calidad y consistencia de los
+            mismos.
+          </p>
+          <br></br>
+          <ul className="flex gap-10 justify-center">
+            <li className="hover:text-yellow-500">
+              <a href="https://github.com/pinzaivan/validador_castastro_python.git">
+                VALIDADOR - Git-Hub
               </a>
             </li>
           </ul>
